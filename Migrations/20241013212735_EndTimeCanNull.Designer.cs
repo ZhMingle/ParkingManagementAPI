7fe4c56a79f3721472a1d550e86714bf39bf0658
Mingle <1179912194@qq.com>
2024-10-14 10:36:12 +1300

    EndTimeNull

--- Migrations/20241013212735_EndTimeCanNull.Designer.cs
+++ Migrations/20241013212735_EndTimeCanNull.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParkingManagementAPI.Data;
 
@@ -11,9 +12,11 @@ using ParkingManagementAPI.Data;
 namespace ParkingManagementAPI.Migrations
 {
     [DbContext(typeof(SmartParkingContext))]
-    partial class SmartParkingContextModelSnapshot : ModelSnapshot
+    [Migration("20241013212735_EndTimeCanNull")]
+    partial class EndTimeCanNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
